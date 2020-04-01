4efb4bcf2db747b487d4363f445b204d54714daa
Moneexa <14besemsyed@seecs.edu.pk>
2020-04-01 18:40:50 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import './App.css';
+import HangMan from './component/HangMan'
 
+function App() {
+  return (
+
+    <div className='container'>
+      <HangMan />
+    </div>
+  );
+}
+
+export default App;
